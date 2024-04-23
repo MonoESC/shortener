ca5126c7af91c26b1eac8af14db40f05e50304ba
Mono <mono.esc@outlook.com>
2024-04-23 09:29:31 -0500

    Update db structure

--- prisma/migrations/20240423142920_init/migration.sql
+++ prisma/migrations/20240423142920_init/migration.sql
@@ -1,11 +1,11 @@
 -- CreateTable
 CREATE TABLE "Redirection" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "code" TEXT NOT NULL,
+    "urlCode" TEXT NOT NULL,
     "url" TEXT NOT NULL,
     "name" TEXT,
     "description" TEXT
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Redirection_code_key" ON "Redirection"("code");
+CREATE UNIQUE INDEX "Redirection_urlCode_key" ON "Redirection"("urlCode");
--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -2,45 +2,45 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const main = async () => {
-  const redirect1 = await prisma.redirection.upsert({
-    where: {
-      code: "code1",
-    },
-    update: {},
-    create: {
-      code: "code1",
-      url: "https://targetweb.com/code1",
-    },
-  });
-  const redirect2 = await prisma.redirection.upsert({
-    where: {
-      code: "code2",
-    },
-    update: {},
-    create: {
-      code: "code2",
-      url: "https://targetweb.com/code2",
-    },
-  });
-  const redirect3 = await prisma.redirection.upsert({
-    where: {
-      code: "code3",
-    },
-    update: {},
-    create: {
-      code: "code3",
-      url: "https://targetweb.com/code3",
-    },
-  });
-  console.log(redirect1, redirect2, redirect3);
+    const redirect1 = await prisma.redirection.upsert({
+        where: {
+            urlCode: "code1",
+        },
+        update: {},
+        create: {
+            urlCode: "code1",
+            url: "https://targetweb.com/code1",
+        },
+    });
+    const redirect2 = await prisma.redirection.upsert({
+        where: {
+            urlCode: "code2",
+        },
+        update: {},
+        create: {
+            urlCode: "code2",
+            url: "https://targetweb.com/code2",
+        },
+    });
+    const redirect3 = await prisma.redirection.upsert({
+        where: {
+            urlCode: "code3",
+        },
+        update: {},
+        create: {
+            urlCode: "code3",
+            url: "https://targetweb.com/code3",
+        },
+    });
+    console.log(redirect1, redirect2, redirect3);
 };
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
